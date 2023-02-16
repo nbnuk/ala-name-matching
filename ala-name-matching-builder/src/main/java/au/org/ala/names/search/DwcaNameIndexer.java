830d5bfe7636196173507054d9a9e5f2ffa7f706
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-02-16 22:37:35 +0100

    fix - ATLASDEV-1023 - common name priority

--- ala-name-matching-builder/src/main/java/au/org/ala/names/search/DwcaNameIndexer.java
+++ ala-name-matching-builder/src/main/java/au/org/ala/names/search/DwcaNameIndexer.java
@@ -386,7 +386,7 @@ public class DwcaNameIndexer extends UksiNameIndexer {
             String taxonID = record.id();
             String vernacularName = record.value(DwcTerm.vernacularName);
             String language = record.value(DcTerm.language);
-            String priority = record.value(ALATerm.priority);
+            String priority = record.value(ALATerm.status);
             String commonNameID = record.value(DwcTerm.scientificNameID);
             TopDocs result = getLoadIdxResults(null, "lsid", taxonID, 1);
             if(result.totalHits.value > 0){
